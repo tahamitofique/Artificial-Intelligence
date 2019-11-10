4fb560c47195e229401b0a94640cc81dd536865b
TahamiTofique <tahamitofique@hotmail.com>
2019-11-10 21:08:44 +0500

    brainjs

---
+++ brain.js/index.js
@@ -0,0 +1,21 @@
+const brain = require ('brain.js');
+const network=new brain.NeuralNetwork();
+
+network.train([
+    { input:[1,2], output: [1]},    //team 2
+    { input:[1,3], output: [1]},    //team 3
+    { input:[2,3], output: [0]},    //team 2
+    { input:[2,4], output: [1]},    //team 4
+    { input:[1,2], output: [1]},    //team 2
+    { input:[1,3], output: [1]},    //team 3
+    { input:[3,4], output: [0]},    //team 3
+    { input:[1,4], output: [0]}     //team 1
+]);
+
+const val={
+    a:3,
+    b:4
+};
+const output=network.run([val.a,val.b]);
+
+console.log(`Winner team: ${output>0.5?val.b:val.a} ...${output}`);
---
+++ brain.js/index2.js
@@ -0,0 +1,16 @@
+const brain = require('brain.js');
+const data = require('./data.json');
+
+const network = new brain.recurrent.LSTM();
+
+const trainingData = data.map(item => ({
+    input:item.text,
+    output:item.category
+}));
+
+network.train(trainingData,{
+    iterations:200
+});
+
+const output=network.run('pendrive');
+console.log(`category: ${output}`);
